74cdc119bbd3aa359c05afd94530747efabe53bf
Maxim Obrubov <m.obrubov@yandex.ru>
2019-05-13 17:56:49 +0400

    Cs 167 sin (#1)
    * CS-167: sinopia changed auth response

--- index.js
+++ index.js
@@ -2,74 +2,68 @@ const _request = require( 'request' );
 const crypto = require( 'crypto' );
 
 function SinopiaRequest( config, sinopia ) {
-
-    var base_options = {
-        method: config.method,
-        header: {
-            'Content-Type': 'application/json',
-            'User-Agent': sinopia.config.user_agent
-        }
-    };
-
-    var encrypt;
-    switch ( config.password_encrypt ) {
-        case 'sha1':
-            encrypt = function ( str ) {
-                return crypto.createHash( 'sha1' ).update( str ).digest( 'hex' );
-            };
-            break;
-        default:
-            encrypt = function ( str ) {
-                return str;
-            };
+  var base_options = {
+    method: config.method,
+    header: {
+      'Content-Type': 'application/json',
+      'User-Agent': sinopia.config.user_agent
     }
+  };
 
-    console.log('sinopia-request plugin loaded.');
+  var encrypt;
+  switch (config.password_encrypt) {
+    case 'sha1':
+      encrypt = function (str) {
+        return crypto.createHash('sha1').update(str).digest('hex');
+      };
+      break;
+    default:
+      encrypt = function (str) {
+        return str;
+      };
+  }
 
-    var request = function ( user, password, route, callback ) {
+  console.log('uchiru/sinopia-request plugin loaded.');
 
-        var options = base_options;
-        options.url = (sinopia.config.https.enable ? 'https' : 'http') + '://' + config.host + (config.port ? ':' + config.port : '') + route;
-        options.json = {
-            user: user,
-            password: encrypt( password )
-        };
-
-        _request( options, function ( err, res, body ) {
-
-            if ( err ) {
-                err.status = 500;
-                return callback( err );
-            }
-            else if ( body === undefined || body.result === undefined ) {
-                err = new Error( 'Bad server response.' );
-                err.status = 500;
-                return callback( err );
-            }
-            else
-                return callback( null, body.result );
-
-        } );
-
+  var request = function (user, password, route, callback) {
+    var options = base_options;
+    options.url = (sinopia.config.https.enable ? 'https' : 'http') + '://' + config.host + (config.port ? ':' + config.port : '') + route;
+    options.json = {
+      user: user,
+      password: encrypt(password)
     };
 
-    this.adduser = function ( user, password, callback ) {
-        return request( user, encrypt( password ), config.adduser_route, callback );
-    };
+    _request(options, function (err, res, body) {
+      if (err) {
+        err.status = 500;
+        return callback( err );
+      } else if (body === undefined || body.result === undefined) {
+        err = new Error('Bad server response.');
+        err.status = 500;
+        return callback(err);
+      } else {
+        return callback(null, body);
+      }
+    });
+  };
 
-    this.authenticate = function ( user, password, callback ) {
-        request( user, encrypt( password ), config.authenticate_route, function ( err, res ) {
-            if ( err )
-                return callback( err );
-            else if ( !res )
-                return callback( null, false );
-            else
-                return callback( null, user );
-        } );
-    };
+  this.adduser = function (user, password, callback) {
+    return request(user, encrypt(password), config.adduser_route, callback);
+  };
 
+  this.authenticate = function (user, password, callback) {
+    request(user, encrypt(password), config.authenticate_route, function (err, response) {
+      if (err) {
+        return callback(err);
+      } else if (response && response.result && response.groups) {
+        return callback(null, response.groups);
+      } else {
+        return callback(null, false);
+      }
+    });
+  };
 }
 
-module.exports = function ( config, stuff ) {
-    return new SinopiaRequest( config, stuff );
+module.exports = function (config, stuff) {
+  return new SinopiaRequest(config, stuff);
 };
